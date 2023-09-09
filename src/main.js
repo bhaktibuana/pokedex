315c6ae4faae6aaaf7b0599b16e407739ba7ea8b
bhaktibuana <bhaktibuana19@gmail.com>
2023-09-09 22:26:52 +0700

    install fontAwesome icon

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import VueLazyload from "vue-lazyload";
+import { FontAwesomeIcon } from "@/libs";
 
-createApp(App).use(store).use(router).use(VueLazyload).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueLazyload)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
