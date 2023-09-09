d01645f6784d7bd161a42de14afd9dd30f5fd38f
bhaktibuana <bhaktibuana19@gmail.com>
2023-09-09 22:27:33 +0700

    fix bug on router chunk

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
     path: "/",
     name: "home",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/HomeView"),
+      import(/* webpackChunkName: "home" */ "../views/HomeView"),
   },
 ];
 
