1d990cbd2e7cfacbe1941ed48e1e4c6bb5b0f8df
bhaktibuana <bhaktibuana19@gmail.com>
2023-09-11 00:54:11 +0700

    cinfigure pwa setting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,46 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pwa: {
+    name: "PokéDex",
+    themeColor: "#ffffff",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "default",
+    manifestOptions: {
+      start_url: ".",
+      display: "standalone",
+      background_color: "#ffffff",
+      version: "0.1.0",
+      icons: [
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    },
+  },
+});
